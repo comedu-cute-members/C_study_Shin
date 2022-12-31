cd268bbccee600b3457373c3c01efd7a19d16ff7
Shin714 <107873352+Shin714@users.noreply.github.com>
2023-01-01 02:59:09 +0900

    [Silver V] Title: 영화감독 숌, Time: 632 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,15 @@
+count = 0
+num = 0
+
+N = int(input(""))
+
+
+while True:
+    num += 1
+    
+    if "666" in str(num):
+        count += 1
+        
+    if count == N:
+        print(num)
+        break
